af936d6ab4a5960549a12fe652c3bb7090a17a5a
gabrielamb2k <gambomfim9@gmail.com>
2025-03-31 19:51:29 -0300

    corrige erros de escrita e erros de retorno no arquivo bulletFuncional.js

--- BulletFuncional.js
+++ BulletFuncional.js
@@ -1,24 +1,24 @@
 //funcao para criar a bala
-const createBullet = (canvas) => (x) => (y) => (velocity) => (bulletColor) => {
+const createBullet = (canvas) => (x) => (y) => (velocity) => (bulletColor) => ({
   canvas,
   x,
   y,
   velocity,
   bulletColor,
-  width = 5,
-  height = 20
-}
+  width:  5,
+  height: 20
+})
 
 //funcao para mover a bala
-const moveBullet = (bullet) => {
-  [...bullet],
-  y = bullet.y - bullet.velocity
-} 
+const moveBullet = (bullet) => ({
+  ...bullet,
+  y: bullet.y - bullet.velocity
+})
 
 //funcao para desenhar a bala
 const drawBullet = (ctx) => (bullet) => {
   ctx.fillStyle = bullet.bulletColor
-  ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.heigth)
+  ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height)
   return bullet
 }
 
@@ -27,8 +27,8 @@ const checkCollission = (bullet) => (sprite) => {
   if(
     bullet.x + bullet.width > sprite.x &&
     bullet.x < sprite.x + sprite.width &&
-    bullet.y + bullet.heigth > sprite.y &&
-    bullet.y < sprite.y + sprite.heigth 
+    bullet.y + bullet.height > sprite.y &&
+    bullet.y < sprite.y + sprite.height 
   ) {
     return true
   }else {
