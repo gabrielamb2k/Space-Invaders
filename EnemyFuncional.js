6c89ebed093aa6e636c7e744bb445bb02c97af40
Pedro <pedro1306@academico.ufs.br>
2025-04-06 17:37:48 -0300

    Update EnemyFuncional.js
    atualizando algumas coisas, alguns erros quando foi rodar,

--- EnemyFuncional.js
+++ EnemyFuncional.js
@@ -1,15 +1,22 @@
  //transformar em const, utilizar curry
-  const createEnemy= (x) =>  (y) => (imageNumber)=> {
-//colocar aimagem e nova imagem no comeco
+
+//fazer uma funcao para a iomgame e uma para  o enemy
+const createImage = (imagemNumber)_ => {
+ //colocar aimagem e nova imagem no comeco
     const image = new novaimagem();
     image.src = `images/enemy${imageNumber}.png`;
-    
+ return image;
+}
+
+  const createEnemy= (x) =>  (y) => (imageNumber)=> {
+//colocar aimagem e nova imagem no comeco
+    const image = createImage(imageNumber);
     return{
     x: x, //retirar os this, e poo, trocar por :
     y: y,
     width:  44,
     height : 32;
-    image : image
+    image
   };
 };
 
@@ -21,25 +28,22 @@
   };
 
 //mover inimigos
-  const move = (enemy)=>(xVelocity) => (yVelocity) => {
+  const move = (enemy) => (xVelocity) => (yVelocity) => {
       //extrair o numero da imagem
+   const imageScr = enemy.image.scr;
     const imageNumber = 
-      enemy.image.src.split('enemy')[1].split('.')[0];
-   //cria um  novo inimigo
+     imageScr.split('enemy')[1]?.split('.')[0] || '1';
+   //retorna um  njovo inimigo com coordenadas atualizadas
     return createEnemy(enemy.x += xVelocity)(enemy.y += yVelocity)(imageNumber);
   };
 
 //detecta colisoes
   collideWith = (enemy) => (sprite) => {
-    if (
+    return (
       enemy.x + enemy.width > sprite.x &&
       enemy.x < sprite.x + sprite.width &&
       enemy.y + enemy.height > sprite.y &&
       enemy.y < sprite.y + sprite.height
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-};
+    );
+  };
 export { createEnemy,draw,move,collideWith };
